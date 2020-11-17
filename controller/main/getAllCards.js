f7cfaad6411258db6ef79f7abbfaae3b710806d9
Gwon92 <xxcvdir@gmail.com>
2020-11-17 10:01:14 +0900

    feature/ making getAllCards

--- controller/main/getAllCards.js
+++ controller/main/getAllCards.js
@@ -8,12 +8,65 @@ module.exports={
         // const {id} = req.user;        
         
         let data = await card.findAll({
+            attributes: ['id','type','title','text',],
             where:{
-                date: moment().format("DD/MM/YYYY")
+                date: moment().format("DD/MM/YYYY"),                
             }
         })
         data = data.map(el => el.get({ plain: true }))
-        console.log(data)
+        if(!data){
+            res.status(200).send({})
+        }
+       
+        for(let i = 0; i < data.length; i++){
+            if(data[i].type === 'toDo'){
+                let todoData = await todolist.findAll({
+                    attributes: ['id','task','isDone',],
+                    where:{
+                        cardId:data[i].id,                
+                    }
+                })
+                todoData = todoData.map(el => el.get({ plain: true }))
+                
+                let temp ={};
+                for(let i = 0; i < todoData.length; i++){
+            
+                  temp[todoData[i].id] = {
+                        task: todoData[i].task,
+                        isDone:todoData[i].isDone,   
+                    }
+                }
+                
+              data[i].content = temp            
+              
+            }
+        }
+                
+        let cards = {}
+
+        for(let i = 0; i < data.length; i++){
+            if(data[i].type === 'note'){
+                cards[data[i].id] = {
+                    type: data[i].type,
+                    title:data[i].title,
+                    text: data[i].text,  
+                }
+            }
+            
+            if(data[i].type === 'toDo'){
+                cards[data[i].id] = {
+                    type: data[i].type,
+                    title:data[i].title,
+                    text: data[i].text,  
+                    content: data[i].content
+                }
+            }
+            
+
+        }
+      
+        
+        res.send({cards:cards})
 
 
     }
--- migrations/20201110113201-create-todolist.js
+++ migrations/20201110113201-create-todolist.js
@@ -12,7 +12,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       isDone: {
-        type: Sequelize.BOOLEAN
+        type: Sequelize.STRING
       },
       createdAt: {
         allowNull: false,
